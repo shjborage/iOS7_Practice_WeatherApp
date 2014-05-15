a1f63b1af731916e8a43aef5169d4c236d931fd6
Eric <shjborage@gmail.com>
2014-05-15 17:07:19 +0800

    Finish the WXClient with RAC

--- SimpleWeather/SimpleWeather/Core/WXClient.h
+++ SimpleWeather/SimpleWeather/Core/WXClient.h
@@ -6,8 +6,17 @@
 //  Copyright (c) 2014 Saick. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+@import Foundation;
+@import CoreLocation;
+#import <ReactiveCocoa/ReactiveCocoa/ReactiveCocoa.h>
+#import "WXCondition.h"
+#import "WXDailyForecast.h"
 
 @interface WXClient : NSObject
+
+- (RACSignal *)fetchJSONFromURL:(NSURL *)url;
+- (RACSignal *)fetchCurrentConditionsForLocation:(CLLocationCoordinate2D)coordinate;
+- (RACSignal *)fetchHourlyForecastForLocation:(CLLocationCoordinate2D)coordinate;
+- (RACSignal *)fetchDailyForecastForLocation:(CLLocationCoordinate2D)coordinate;
 
 @end
